55fc35be5ef1fc4e0c2cb31b524b778a0a611578
RamseyAAK <RamseyAAK@gmail.com>
2026-06-10 13:37:01 -0700

    oops, missed framebuffer.js

--- framebuffer.js
+++ framebuffer.js
@@ -1,7 +1,7 @@
 import { getFileAsString, createSimpleProgram,
          sliderInput, timeInput, resolutionInput, 
          dragInput, createFrameBuffer,
-         clickInput, setupCanvas}
+         mouseInput, setupCanvas}
   from './shader_setup.js';
 
 document.querySelectorAll('.framebuffer_project').forEach(x => { assignShader(x)});
@@ -31,21 +31,24 @@ async function assignShader(project) {
   // gl.enable(gl.DEPTH_TEST); 
   // gl.depthFunc(gl.LEQUAL); 
 
-  const vsSource = `#version 300 es
-    layout(location=0) in vec4 aVertexPos;
-    void main() {
-      gl_Position = aVertexPos;
-    }
-  `;
+  const vsSource = 
+  `#version 300 es
+  layout(location=0) in vec4 aVertexPos;
+  void main() {
+    gl_Position = aVertexPos;
+  }`;
 
-  const fsDisplay = `#version 300 es
-    precision lowp float;
-    uniform sampler2D buff;
-    layout(location=0) out vec4 finalColor;
-    void main() {
-      finalColor = texelFetch(buff, ivec2(gl_FragCoord.xy), 0);
-    }
-  `;
+  let fsDisplay = await getFileAsString(project.dataset.view);
+  if (!(fsDisplay)) {
+    fsDisplay = 
+  `#version 300 es
+  precision lowp float;
+  uniform sampler2D buff;
+  layout(location=0) out vec4 finalColor;
+  void main() {
+    finalColor = texelFetch(buff, ivec2(gl_FragCoord.xy), 0);
+  }`;
+  }
 
   const fsSource = await getFileAsString(project.dataset.frag);
 
@@ -60,7 +63,7 @@ async function assignShader(project) {
   //_____________________________________________________________________________
 
   // Configure Calculate and Draw Functions: ---------------------------------------------------
-  const ITERATIONS = 3;
+  const ITERATIONS = 6;
   function drawAndCalc(gl) {
     for (let i = 0; i < ITERATIONS; ++i) {
       calculate(gl, pCalculate, fbA, fbB, textureA, textureB);
@@ -73,11 +76,17 @@ async function assignShader(project) {
   if (fsSource.includes('iResolution')) {
     resolutionInput(gl, pCalculate, canvas);
   }
+  if (fsDisplay.includes('iResolution')) {
+    resolutionInput(gl, pDisplay, canvas);
+  }
   //_____________________________________________________________________________
 
   // Slider input: --------------------------------------------------------------
   if (fsSource.includes('iSlider')) {
     sliderInput(gl, pCalculate, document, showcase, drawAndCalc, false);
+  }
+  if (fsDisplay.includes('iSlider')) {
+    sliderInput(gl, pDisplay, document, showcase, drawAndCalc, false);
   }
   //_____________________________________________________________________________
 
@@ -88,8 +97,8 @@ async function assignShader(project) {
   //_____________________________________________________________________________
 
   // Click input: --------------------------------------------------------
-  if (fsSource.includes('iClick')) {
-    clickInput(gl, pCalculate, canvas, drawAndCalc, false);
+  if (fsSource.includes('iMouse')) {
+    mouseInput(gl, pCalculate, canvas, drawAndCalc, false);
   }
   //_____________________________________________________________________________
 
@@ -104,25 +113,48 @@ async function assignShader(project) {
   code.textContent = fsSource;
 
   hljs.highlightElement(code);
-//_____________________________________________________________________________
+  //_____________________________________________________________________________
 
   // Draw: ----------------------------------------------------------------------
   // Just gotta draw once here so that the shaders start off visible
   drawAndCalc(gl);
   //_____________________________________________________________________________
+
+  // Button: --------------------------------------------------------------------
+  const buttonScript = await getFileAsString(project.dataset.button);
+  if (buttonScript) {
+    let label = document.createElement('label');
+    label.textContent = project.dataset.button;
+    let button = document.createElement('button');
+    // button.setAttribute('width', '24px');
+    // button.setAttribute('height', '24px');
+    button.setAttribute('class', 'showcase_button');
+    button.appendChild(label);
+    showcase.appendChild(button);
+    const pButton = createSimpleProgram(gl, vsSource, buttonScript);
+    button.addEventListener('click', () => {
+      gl.useProgram(pButton);
+      gl.bindTexture(gl.TEXTURE_2D, textureA);
+      gl.bindFramebuffer(gl.FRAMEBUFFER, fbB);
+      gl.drawArrays(gl.TRIANGLES, 0, 3);
+    });
+  }
+  //_____________________________________________________________________________
 }
 
-function calculate(gl, pCalc, fbA, fbB, textureA, textureB) {
+
+
+function calculate(gl, pCalc, fbA, fbB, texA, texB) {
   // Draw from buffer to buffer
   gl.useProgram(pCalc);
   // Assign buffer B as input
-  gl.bindTexture(gl.TEXTURE_2D, textureB)
+  gl.bindTexture(gl.TEXTURE_2D, texB);
   // Assign buffer A as output
   gl.bindFramebuffer(gl.FRAMEBUFFER, fbA);
   // Draw
   gl.drawArrays(gl.TRIANGLES, 0, 3);
   // Assign buffer A as input
-  gl.bindTexture(gl.TEXTURE_2D, textureA);
+  gl.bindTexture(gl.TEXTURE_2D, texA);
   // Assign buffer B as output
   gl.bindFramebuffer(gl.FRAMEBUFFER, fbB);
   // Draw
